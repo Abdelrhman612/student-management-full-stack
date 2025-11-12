b307cdf20750a18bc20dd1fbc7029893ad069bb1
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2025-11-12 10:24:21 +0200

    feat: create 20251112072902_changeNameCloumn.Designer.cs, 20251112072902_changeNameCloumn.cs, 20251112080806_AddDefaultValue.Designer.cs and 20251112080806_AddDefaultValue.cs

---
+++ backend/Migrations/20251112072902_changeNameCloumn.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace student_management.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeNameCloumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "Student",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true,
+                oldDefaultValue: "student");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "student",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true,
+                oldDefaultValue: "Student");
+        }
+    }
+}
---
+++ backend/Migrations/20251112080806_AddDefaultValue.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace student_management.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 2,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 2);
+        }
+    }
+}
